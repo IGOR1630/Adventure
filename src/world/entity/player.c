acef00403bf71f7a903837388a9c68fef3e31489
Jonatha Gabriel <jonathagabrielns@gmail.com>
2022-10-25 21:26:29 -0300

    Aviso de copyright atualizado.

--- src/world/entity/player.c
+++ src/world/entity/player.c
@@ -1,7 +1,7 @@
 /*
 The GPLv3 License (GPLv3)
 
-Copyright (c) 2022 Jonatha Gabriel
+Copyright (c) 2022 Jonatha Gabriel <jonathagabrielns@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
